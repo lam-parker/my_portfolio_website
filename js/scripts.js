a8351949924fbe634b4dd9f071079007f6795414
lam-parker <lam-parker@users.noreply.github.com>
2015-09-30 21:21:29 -0400

    added js to make the gifs animate when you scroll
    to that part of the page

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,7 @@ var BOOL = false;
 $(window).scroll(function() {
 
 
+
   var current = $(this).scrollTop();
   // path = 'http://www.anony.ws/i/2015/09/25/text.gif',
   // path = $(this).find('img').attr('src');
