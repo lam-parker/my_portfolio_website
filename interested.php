121954d1ebfc8bcede1f93c09c18235061024489
lam-parker <lamara.parker@gmail.com>
2016-03-15 20:53:14 -0400

    changed file name and added php

---
+++ interested.php
@@ -0,0 +1,77 @@
+<!DOCTYPE php>
+<html lang="en">
+ <head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+   <title>Lamara Parker | Front-End Developer</title>
+   <link href="img/favicon.ico" rel="shortcut icon">
+
+   <!-- Bootstrap -->
+   <link href="css/bootstrap.min.css" rel="stylesheet">
+   <link rel="stylesheet" href="css/normalize.css">
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans:300|PT+Sans:400,700,700italic' rel='stylesheet' type='text/css'>
+   <link rel="stylesheet" href="css/main.css">
+
+   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+   <!--[if lt IE 9]>
+     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+   <![endif]-->
+ </head>
+ <body>
+   <!-- /NAVIGATION SECTION -->
+<nav class="navbar navbar-default navbar-static-top">
+   <div class="container">
+     <div class="navbar-header">
+       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+         <span class="sr-only">Toggle navigation</span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+       </button>
+       <a class="navbar-brand" href="index.html">Lamara Parker</a>
+     </div>
+     <div id="navbar" class="navbar-collapse collapse">
+       <ul class="nav navbar-nav navbar-right">
+         <li><a href="index.html">Home</a></li>
+         <li><a href="proexp.html">Professional Experience</a></li>
+         <li><a href="edu.html">Education</a></li>
+         <li><a href="sofskills.html">Software Skills</a></li>
+         <li><a href="deskills.html">Design Skills</a></li>
+         <li class="active"><a href="interested.php" class="selected">Interested</a></li>
+       </ul>
+     </div><!--/.nav-collapse -->
+   </div>
+ </nav>
+
+   <!-- END of NAVIGATION SECTION/ -->
+   <div class="jumbotron">
+     <h1 class="ss-title">Write Me</h1><br>
+     <div class="form-group">
+      <label class="col-md-4"></label>
+       <div class="col-md-4 center-block">
+       </div>
+     </div>
+   </div>
+<?php
+
+  echo "Form Goes Here";
+
+?>
+     <div id="wrapper">
+       <footer>
+         <a href="http://facebook.com" target="_blank"><img src="img/footer/facebook-logo.svg" alt="Facebook Logo"></a>
+         <a href="https://www.linkedin.com/in/lamaraparker" target="_blank"><img src="img/footer/linkedin-logo.svg" alt="LinkedIn Logo"></a>
+         <p>&copy; 2015 Lamara Parker.</p>
+       </footer>
+     </div>
+
+   <!-- Bootstrap jQuery PlugIns -->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+   <!-- Include all compiled plugins (below), or include individual files as needed -->
+   <script src="js/bootstrap.min.js"></script>
+ </body>
+</html>
