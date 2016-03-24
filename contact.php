b51d0744db60b58fd9038e74a8b45537a58e3f8a
lam-parker <lamara.parker@gmail.com>
2016-03-23 20:08:46 -0400

    changed curly brackets to parenthases

--- contact.php
+++ contact.php
@@ -1,5 +1,7 @@
 <?php
-var_dump {$_POST};
+
+var_dump ($_POST);
 //redirect to thank you page
-//submit an confirmation email to me  
+//submit an confirmation email to me
+//if information is not entered properly send to error page
  ?>
